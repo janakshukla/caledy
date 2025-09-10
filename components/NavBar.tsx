4856bc6e8eafca98aaa24451e09696f2b38e6e4e
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-09-10 14:21:13 +0530

    Add logo image to NavBar and adjust layout for branding

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,13 +10,16 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { SignInGithub, SignInGoogle } from "./AuthButtons";
+import Image from "next/image";
+import Logo from "@/public/Caledy.png"
 
 const NavBar = () => {
   return (
     <nav className=" flex justify-between bg-background  p-4">
       <div>
-        <h1 className="text-2xl font-bold text-primary">
-          Ca<span className="text-foreground">ledy</span>{" "}
+        <h1 className="text-2xl flex gap-2 font-bold text-primary">
+          <Image src={Logo} alt="logo" className="size-10 rounded-full "  />
+          Ca<span className=" -ml-2 text-foreground">ledy</span>{" "}
         </h1>
       </div>
       <Dialog>
